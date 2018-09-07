963792f58f7d6696e80efbd3fb0fbe97dfde34b9
yves songolo <yves.songolo@gmail.com>
2018-09-06 19:07:50 -0700

    user can see all reviews

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
 const app = express()
-app.engine('hbs',exphbs({defaultLayout:'main'}));
-app.set('view engine','hbs');
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine','handlebars');
 
 
 
 
+// ROOT
 
 app.get('/', (req,res) => {
-    res.send("we are live")
+    res.render('home', {msg:'we are live...'})
 });
+// index reviews
+app.get('/reviews',(req,res)=>{
+    res.render('reviews-index',{reviews:reviews});
+})
+
+
 app.listen(3000, () =>{
     console.log('App listening on port 3000!')
 });
 
+// mock array
+let reviews = [
+    {title: 'Great Review'},
+    {title: 'Next Review'}
+]
