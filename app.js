8a83f56c8616396efda6a8a1be855a61651fc4c0
yves songolo <yves.songolo@gmail.com>
2018-09-08 15:04:09 -0700

    adding some root

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.engine('handlebars',exphbs({defaultLayout:'main'}));
 app.set('view engine','handlebars');
 
 
+// connect mongoose db
+mongoose.connect('mongodb://localhost/rotten-potatoes',{
+   // useMongoClient: true
+   useNewUrlParser: true
+});
+
+// create a review model
+const Review = mongoose.model('Review',{
+    title: String
+});
 
 
 // ROOT
-
 app.get('/', (req,res) => {
-    res.render('home', {msg:'we are live...'})
-});
+    Review.find()
+    .then(reviews => {
+        res.render('reviews-index',{reviews:reviews});
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+})
+
+// CREATE
+app.post('/reviews',(req,res)=>{
+   Review.create(req.body)
+   .then((review) => {
+    console.log(review);
+    res.redirect('/');
+   })
+   .catch((err) =>{
+    console.log(err.message);
+   })
+})
 // index reviews
 app.get('/reviews',(req,res)=>{
     res.render('reviews-index',{reviews:reviews});
 })
 
+// new review
+app.get('/reviews/new',(req,res)=>{
+    res.render('reviews-new',{});
+})
 
 app.listen(3000, () =>{
     console.log('App listening on port 3000!')
 });
 
-// mock array
-let reviews = [
-    {title: 'Great Review'},
-    {title: 'Next Review'}
-]
+
+
