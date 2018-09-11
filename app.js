296c71c436dcf96034a1de8430db37867799ddc4
yves songolo <yves.songolo@gmail.com>
2018-09-11 13:56:57 -0700

    some changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // connect mongoose db
-mongoose.connect('mongodb:https://rotten-potatoes-back-end-app.herokuapp.com',{
+mongoose.connect('mongodb://localhost/rotten-potatoes',{
    useNewUrlParser: true
 });
 
